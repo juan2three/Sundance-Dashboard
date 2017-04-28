297276e71bbe5ef3c7b2688af11637ad2d518ecc
juan2three <sk8erjuan@gmail.com>
2017-04-27 22:12:48 -0400

    Update birthdays.php

--- birthdays.php
+++ birthdays.php
@@ -1,4 +1,14 @@
 
+ 	
+PHP Beautifier
+Beautify and format your PHP code
+ 
+Here is your PHP code :
+ 	
+ Source
+ 
+See as Text 	
+
 <?php
 $page = $_SERVER['PHP_SELF'];
 $sec = "10";
@@ -6,7 +16,8 @@ $sec = "10";
 <!doctype html>
 <html>
 <head>
-<meta http-equiv="refresh" content="<?php echo $sec?>;URL='events.php'">
+<meta http-equiv="refresh" content="<?php
+echo $sec ?>;URL='events.php'">
  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
  <title>Birthdays</title>
  <meta charset="UTF-8">
@@ -36,50 +47,37 @@ $sec = "10";
                 </div>
 
                 <div class="text-center" id="event-list"><?php
+$url = 'https://www.googleapis.com/calendar/v3/calendars/9hqtea8k8rlin07vfolg1oqs9g%40group.calendar.google.com/events?key=AIzaSyBChWJmNLkmntozxqjWvJVPplApVLEGzLc';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$output = curl_exec($ch);
+curl_close($ch);
+$data = json_decode($output, true);
+$jason = json_decode($output, true);
+echo "<div id='list'>";
+$today = date('Y-m-d');
+$cnt = 0;
 
-                    $url = 'https://www.googleapis.com/calendar/v3/calendars/9hqtea8k8rlin07vfolg1oqs9g%40group.calendar.google.com/events?key=AIzaSyBChWJmNLkmntozxqjWvJVPplApVLEGzLc';
-                    $ch = curl_init();  
+foreach($jason['items'] as $item)
+	{
+	if (preg_match("/$today/", $item['start']['date']))
+		{
+		if (preg_match('/Birthday:/', $item['summary']))
+			{
+			echo str_replace("Birthday:", "", $item['summary']);
+			echo "<br />";
+			$cnt++;
+			}
+		}
+	}
 
-                    curl_setopt($ch,CURLOPT_URL,$url);
-                    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+if ($cnt == 0)
+	{
+	echo "<script language='Javascript'>document.location.replace('events.php');</script>";
+	}
 
-
-                    $output=curl_exec($ch);
-
-                    curl_close($ch);
-
-                    $data = json_decode($output,true);
-
-
-                    $jason = json_decode($output,true);
-
-                    echo "<div id='list'>";
-
-
-                    $today = date('Y-m-d');  
-
-
-                    $cnt = 0;
-
-
-
-                    foreach ($jason['items'] as $item) {
-
-                        if (preg_match("/$today/", $item['start']['date'] ) ) {
-
-                            if (preg_match('/Birthday:/', $item['summary'])) {
-                                echo str_replace("Birthday:", "",  $item['summary']);
-                                echo "<br>";
-                                $cnt++;
-                            }
-                        }
-                    }
-
-                    if ($cnt == 0){
-                       echo "<script language='Javascript'>document.location.replace('events.php');</script>";
-                   }
-
-                   ?>
+?>
                </div>
            </div>
 
@@ -91,3 +89,14 @@ $sec = "10";
 </body>
 </html>
 
+
+ 
+ 
+Site Info  |  Disclaimer  |  Privacy 
+Site Network  |  Add to favorites	
+© 2009-2017 PHP Beautifier - All rights reserved 
+ 
+Related terms: programming, coding, php, scripts, dev,
+programmers, code, Zend, Pear, javascript
+ 
+ 
